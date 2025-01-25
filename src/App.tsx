279346688ef0139f269b23c498d57a0b90d609fc
bmkinney6 <bmkinney6@gmail.com>
2025-01-25 10:50:53 -0600

    Fix form styling and nav styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Register from './Pages/register.tsx';
 import NotFound from './Pages/notFound.tsx';
 import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import ProtectedRoute from "./Components/ProtectedRoute.tsx";
-import './App.css'
+
 
 function Logout() {
     localStorage.clear();
@@ -15,30 +15,34 @@ function Logout() {
 }
 
 function RegisterAndLogout() {
-    localStorage.clear();
-    return <Register />;
+  localStorage.clear();
+  return <Register />;
 }
 
 function App() {
   return (
-      <Router>
-          <div>
-              <NavBar />
-              <div className="content">
-                  <Routes>
-                      <Route path="/" element={<Home />} />
-                      <Route path="/about" element={<About />} />
-                      <Route path="/players" element={
-                          <ProtectedRoute><Players /></ProtectedRoute>
-                      } />
-                      <Route path="/login" element={<Login />}/>
-                      <Route path="/logout" element={<Logout />}/>
-                      <Route path="/register" element={<RegisterAndLogout />}/>
-                      <Route path="*" element={<NotFound />}/>
-                  </Routes>
-              </div>
-          </div>
-      </Router>
+
+    <Router>
+        <NavBar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/players"
+            element={
+              <ProtectedRoute>
+                <Players />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -2,6 +2,7 @@ import {FormEvent, useState} from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import '../Styles/Form.css';
 
 interface FormProps {
     route: string;
@@ -37,26 +38,33 @@ function Form({ route, method }: FormProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
-      <h1>{name}</h1>
-      <input
-        className="form-control-md"
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        className="form-control-md"
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className="btn btn-primary" type="submit">
-        {name}
-      </button>
-    </form>
+      <form onSubmit={handleSubmit} className="form-container text-center">
+          <img //replace with our logo!
+              className="mt-5 rounded-2"
+              src="src/assets/H2HLogo.jpg"
+              width={100}
+              height={100}
+              alt="Logo"
+          />
+          <h1 >{name}</h1>
+          <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+              className="form-control"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn btn-lg btn-primary btn-block mt-3" type="submit">
+              {name}
+          </button>
+      </form>
   );
 }
 
