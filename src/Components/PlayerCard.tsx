90be16e34842e806494d7de7af41b788ec14f3f0
bmkinney6 <bmkinney6@gmail.com>
2025-01-26 13:52:07 -0600

    player card design

--- src/Components/PlayerCard.tsx
+++ src/Components/PlayerCard.tsx
@@ -2,16 +2,22 @@ import "../Styles/Index.css";
 
 export default function PlayerCard() {
   return (
-    <div className="container-sm border border-dark rounded-5">
+    <div
+      id="PlayerCard"
+      className="container-sm d-flex flex-column justify-content-center align-items-center p-3"
+    >
       <img
         src="src/assets/H2HLogo.jpg"
         id="PlayerCardImg"
         className="rounded-circle"
         width={100}
         height={100}
-        alt="Logo"
+        alt="Player Headshot"
       />
-      <h1 className="text-center">Player Card</h1>
+      <h1 className="text-center">Name</h1>
+      <h2 className="text-center">Team</h2>
+      <h2 className="text-center">Position</h2>
+      <h5 className="text-center">Hometown</h5>
     </div>
   );
 }
