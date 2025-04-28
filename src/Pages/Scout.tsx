9c21494b9e39c67b4ea913cfacd92022665db320
Usman Kaleel <u.kaleel7894@gmail.com>
2025-04-27 20:50:33 -0500

    patches 4

--- src/Pages/Scout.tsx
+++ src/Pages/Scout.tsx
@@ -1,13 +1,11 @@
-import PlayerList from "../Components/PlayerList.tsx";
-import PlayerSearchCard from "../Components/PlayerCard";
+
 import PlayerSearchList from "../Components/PlayerSearchList.tsx";
 
 
 export default function Scout() {
   return (
     <div className="container-fluid">
-      {/*<PlayerSearchCard />*/}
-      {/* <PlayerList /> */ }
+    
       <PlayerSearchList />
 
     </div>
